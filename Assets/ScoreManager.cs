cd16c2999373c1e0066c0d30a532e8a502266f34
cph-jm390 <90380942+cph-jm390@users.noreply.github.com>
2024-01-03 16:13:06 +0100

    score

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text totalText;
+    private float scoreCount = 1000f;  // Initial score value
+    private float totalCount = 500f;  // Initial score value
+    private float timeElapsed = 0f;
+    public float subtractInterval = 1f;  // Time interval to subtract score
+
+    void Update()
+    {   
+        // Update the time elapsed
+        timeElapsed += Time.deltaTime;
+
+        // Check if the subtractInterval has passed
+        if (timeElapsed >= subtractInterval)
+        {
+            // Subtract the score
+            subtractScore();
+
+            // Reset the timeElapsed for the next interval
+            timeElapsed = 0f;
+        }
+
+        // Update the score text
+        scoreText.text = "Score: " + Mathf.Round(scoreCount);
+        totalText.text= "Total: "+ totalCount;
+    }
+
+    void subtractScore()
+    {
+        // Subtract 3 from the scoreCount
+        scoreCount -= 3f;
+        if(scoreCount==0){
+            scoreCount =0;
+        }
+    }
+    void totalHandler(){
+        totalCount+=scoreCount;
+    }
+    void scoreReset(){
+        scoreCount=1000f;
+    }
+    void subtractTotal(float price){
+        totalCount-=price;
+    }
+    void addTotal(float currentScore){
+        totalCount+=scoreCount
+    }
+}
